d79e1281e2a68f9a26d79a85e82d1b43786fa00a
Андрей Овчаренко <ovcharenkoav@meta.ua>
2016-06-22 18:49:43 +0300

    next

--- cdb.py
+++ cdb.py
@@ -1,24 +1,33 @@
 import asyncio
+import aiohttp
 import aiocouchdb
 
+
+#auth = aiocouchdb.authn.BasicAuthProvider(name='admin', password='')
 loop = asyncio.get_event_loop()
 test = aiocouchdb.Database('http://localhost:5984/test')
 server = aiocouchdb.Server()
+rest = aiocouchdb.client.Resource('http://localhost:5984/test')
+
+
 
 async def all_dbs():
     dbs = await server.all_dbs()
     test = await server.db('test')
-    docs = await asyncio.ensure_future(test.all_docs())
     res = await test.all_docs()
     while True:
         rec = await res.next()
         if rec is None:
             break
+        print(rec['value']['rev'])
         print(rec)
+        doc = await test.doc(rec['key'])
+        #response = await doc.update({"_id":rec['key'],"www": "rest"})
+        response = await rest.request('PUT', path='ffec2edc824109e77dc77d9f380017c0', data={"_id":'ffec2edc824109e77dc77d9f380017c0',"test++--": "rest++--"})
     print(dbs)
     print(test)
-    print(docs)
-    dir(docs)
+    print(rec)
+    dir(rec)
     print('++++++++++++++++')
 
 
