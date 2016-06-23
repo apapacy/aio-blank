109ac8b8ddea238a90075cfd386617cc8bd5718d
Андрей Овчаренко <ovcharenkoav@meta.ua>
2016-06-23 03:49:59 +0300

    next

---
+++ cass.php
@@ -0,0 +1,65 @@
+<?php
+require 'php-cassandra/php-cassandra.php';
+$nodes = [
+    '127.0.0.1',        // simple way, hostname only
+/*    '192.168.0.2:9042', // simple way, hostname with port
+    [               // advanced way, array including username, password and socket options
+        'host'      => '10.205.48.70',
+        'port'      => 9042, //default 9042
+        'username'  => 'admin',
+        'password'  => 'pass',
+        'socket'    => [SO_RCVTIMEO => ["sec" => 10, "usec" => 0], //socket transport only
+        ],
+    ],
+    [               // advanced way, using Connection\Stream, persistent connection
+        'host'      => '10.205.48.70',
+        'port'      => 9042,
+        'username'  => 'admin',
+        'password'  => 'pass',
+        'class'     => 'Cassandra\Connection\Stream',//use stream instead of socket, default socket. Stream may not work in some environment
+        'connectTimeout' => 10, // connection timeout, default 5,  stream transport only
+        'timeout'   => 30, // write/recv timeout, default 30, stream transport only
+        'persistent'    => true, // use persistent PHP connection, default false,  stream transport only
+    ],
+    [               // advanced way, using SSL
+        'class'     => 'Cassandra\Connection\Stream', // "class" must be defined as "Cassandra\Connection\Stream" for ssl or tls
+        'host'      => 'ssl://10.205.48.70',// or 'tls://10.205.48.70'
+        'port'      => 9042,
+        'username'  => 'admin',
+        'password'  => 'pass',
+    ],*/
+];
+
+// Create a connection.
+$connection = new Cassandra\Connection($nodes, 'mykeyspace');
+
+//Connect
+try
+{
+    $connection->connect();
+}
+catch (Cassandra\Exception $e)
+{
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+    exit;//if connect failed it may be good idea not to continue
+}
+$begin = time();
+try
+{
+  for ($i=0; $i < 10000000;$i++) {
+    $statement = $connection->queryAsync(
+    "INSERT INTO users (user_id,  fname, lname) VALUES ($i, 'john-$i', 'smith-$i')");
+  }
+
+}
+catch (Cassandra\Exception $e)
+{
+  echo 'Caught exception: ',  $e->getMessage(), "\n";
+  exit;//if connect failed it may be good idea not to continue
+}
+//$statement = $connection->querySync('drop table  IF EXISTS  mykeyspace.users');
+$statement = $connection->queryAsync('select count(*) from users');
+$response = $statement->getResponse();
+$rows = $response->fetchAll();
+print_r($rows);
+echo time() - $begin;
--- cdb.py
+++ cdb.py
@@ -7,28 +7,31 @@ import aiocouchdb
 loop = asyncio.get_event_loop()
 test = aiocouchdb.Database('http://localhost:5984/test')
 server = aiocouchdb.Server()
-rest = aiocouchdb.client.Resource('http://localhost:5984/test')
+rest = aiocouchdb.client.Resource('http://admin:password@localhost:5984')
 
 
 
 async def all_dbs():
-    dbs = await server.all_dbs()
+    #dbs = await server.all_dbs()
     test = await server.db('test')
-    res = await test.all_docs()
-    while True:
-        rec = await res.next()
-        if rec is None:
-            break
-        print(rec['value']['rev'])
-        print(rec)
-        doc = await test.doc(rec['key'])
-        #response = await doc.update({"_id":rec['key'],"www": "rest"})
-        response = await rest.request('PUT', path='ffec2edc824109e77dc77d9f380017c0', data={"_id":'ffec2edc824109e77dc77d9f380017c0',"test++--": "rest++--"})
-    print(dbs)
-    print(test)
-    print(rec)
-    dir(rec)
-    print('++++++++++++++++')
+    #res = await test.all_docs()
+    #while True:
+    #    rec = await res.next()
+    #    if rec is None:
+    #        break
+    #    print(rec['value']['rev'])
+    #    print(rec)
+    #    doc = await test.doc(rec['key'])
+    #    #response = await doc.update({"_id":rec['key'],"www": "rest"})
+    #    response = rest.request('POST', path='', data={"test++--": "rest++--"})
+    #print(dbs)
+    #print(test)
+    #print(rec)
+    #dir(rec)
+    #print('++++++++++++++++')
+    for x in range(1, 1000000):
+        asyncio.ensure_future(rest.request('POST', path='test', data={"test++--": "rest++--"}))
+        #print(response)
 
 
 asyncio.ensure_future(all_dbs())
